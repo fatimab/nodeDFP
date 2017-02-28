e5184d74e3bf50161a483cdce3a3f9a7975c0279
Chabi Fatima <boudieb.fatima@gmail.com>
2017-02-28 14:53:38 +0100

    serveur ok

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ var url = require('url');
 var server = http.createServer(function (requete, reponse) {
 		var urlpage = url.parse(requete.url).pathname;
 		reponse.writeHead(200,{"Content-Type": "text/plain; charset=utf-8"});
-		reponse.end("bye");
+		reponse.end(urlpage);
 });
 
 server.listen(8888);
