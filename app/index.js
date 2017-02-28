6afc82cc1de3f1f3fab8bd2883d8ef4e1df462b4
Chabi Fatima <boudieb.fatima@gmail.com>
2017-02-28 14:51:03 +0100

    correction package.json

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ var url = require('url');
 var server = http.createServer(function (requete, reponse) {
 		var urlpage = url.parse(requete.url).pathname;
 		reponse.writeHead(200,{"Content-Type": "text/plain; charset=utf-8"});
-		reponse.end("");
+		reponse.end("bye");
 });
 
 server.listen(8888);
